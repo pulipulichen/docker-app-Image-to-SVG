5f15f7094dfdb59c8a10bdfa0534f7a5ffdf5fa4
Pulipuli Chen <pulipuli.chen@gmail.com>
2025-06-20 21:18:38 +0800

    20250620.211838

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+ 
+FROM golang:1.24.4-bookworm
+
+# Install Rust and Cargo
+RUN apt-get update && apt-get install -y curl
+
+# Install Node.js and npm
+RUN apt-get install -y nodejs npm # 加上註解：安裝 Node.js 和 npm
+
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Set the PATH environment variable to include Cargo's bin directory
+ENV PATH="/root/.cargo/bin:${PATH}"
+
+RUN cargo install vtracer
+# 加上註解：指定 vtracer 執行的完整路徑
---
+++ app/index.js
@@ -0,0 +1,44 @@
+const ShellSpawn = require('./lib/ShellSpawn')
+const ShellExec = require('./lib/ShellExec')
+const GetFiles = require('./lib/GetFiles')
+
+const path = require('path')
+const fs = require('fs')
+
+// convert a.tif -thumbnail 64x64^ -gravity center -extent 64x64 b.ico
+
+let main = async function () {
+  let files = GetFiles()
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+    
+    let filename = path.basename(file)
+    let dirname = path.dirname(file)
+    let filenameNoExt = path.parse(filename).name
+    let ext = path.extname(filename).toLowerCase()
+
+    if (ext === '.svg') {
+      console.log(`Skipping ${file} as it is already a SVG`)
+      continue
+    }
+
+    const supportedExtensions = [".png", ".jpg", ".jpeg", ".bmp", ".gif", ".tiff", ".webp"];
+
+    if (supportedExtensions.indexOf(ext) === -1) {
+      console.log(`Skipping ${file} as it is not a supported image format`)
+      continue
+    }
+
+    await ShellExec(`cp "${file}" /tmp/input.${ext}`)
+    file = `/tmp/input.${ext}`
+
+    let isJPG = false
+    let isAVIF = false
+
+    dirname = '/output/'
+    await ShellExec(`vtracer --input "${file}" --output "${path.resolve(dirname, filenameNoExt + ".svg")}"`)
+    // convert -gravity center "c.png" -flatten -fuzz 1% -trim +repage -resize 64x64 -extent 64x64 "b.ico"
+  }
+}
+
+main()
